7c07e99b7acd9cbf6d2c4ccbb51218cae665c641
TASconsultorias <contacto@tasconsultorias.com>
2026-05-13 22:41:15 -0400

    Add files via upload

---
+++ styles/comunas_17_4_style.js
@@ -0,0 +1,31 @@
+var size = 0;
+var placement = 'point';
+
+var style_comunas_17_4 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    
+    var labelText = ""; 
+    var value = feature.get("");
+    var labelFont = "14.3px \'Open Sans\', sans-serif";
+    var labelFill = "#98d8da";
+    var bufferColor = "#f1fafa";
+    var bufferWidth = 3.5999999999999996;
+    var textAlign = "left";
+    var offsetX = 0;
+    var offsetY = 0;
+    var placement = 'point';
+    if (feature.get("COMUNA") !== null) {
+        labelText = String(feature.get("COMUNA"));
+    }
+    var style = [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(255,154,0,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 1.748}),fill: new ol.style.Fill({color: 'rgba(240,136,75,0.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+
+    return style;
+};
